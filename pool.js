2b7396d85385caede7823eba10fbe979f942ef06
fyy <fyy17050@163.com>
2019-04-15 18:51:08 +0800

    b

--- pool.js
+++ pool.js
@@ -1,12 +1,11 @@
 const mysql=require('mysql');
 //创建连接池,哪一个模块需要连接，只需引用此模块即可
 var pool=mysql.createPool({
-	host:'127.0.0.1',
-	port:3306,
-	user:'root',
-	password:'',
-	database:'sj',
-	connectionLimit:20
+	host     : process.env.MYSQL_HOST,
+    port     : process.env.MYSQL_PORT,
+    user     : process.env.ACCESSKEY,
+    password : process.env.SECRETKEY,
+    database : 'app_' + process.env.APPNAME
 });
 //导出连接池
 module.exports=pool;
