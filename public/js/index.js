46dd37d28747c5d98407de31c53a5b878be84ff5
fyy <fyy17050@163.com>
2019-04-16 15:13:00 +0800

    t

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 //首页
 //首页轮播
-function add(){
+window.onload=function(){
     var xhr=new XMLHttpRequest();
    xhr.onreadystatechange=function(){
 	   if(xhr.readyState==4 && xhr.status==200){
            var result=JSON.parse(xhr.responseText);
         //    第一个
             var html="";
-	    	for(var i=0;i<result.length-4;i++){
+	    	for(var i=4;i<result.length-0;i++){
 	        	var p=result[i];
 	        	html+=`
                     <a href="${p.href}" target="_blank">
@@ -17,6 +17,17 @@ function add(){
        	}
             var div=document.getElementById("swi");
             div.children[0].innerHTML=html;
+            var html1="";
+	    	for(var i=0;i<result.length-4;i++){
+	        	var p=result[i];
+	        	html1+=`
+                    <a href="${p.href}" target="_blank">
+                        <img src="${p.img}">
+                    </a>
+                    `;
+       	}
+            var div=document.getElementById("swi");
+            div.children[1].innerHTML=html1;
 			var html2="";
 	    	for(var i=1;i<result.length-3;i++){
 	        	var p=result[i];
@@ -27,7 +38,7 @@ function add(){
                     `;
        	}
             var div=document.getElementById("swi");
-            div.children[1].innerHTML=html2;
+            div.children[2].innerHTML=html2;
             var html3="";
 	    	for(var i=2;i<result.length-2;i++){
 	        	var p=result[i];
@@ -38,7 +49,7 @@ function add(){
                     `;
        	}
             var div=document.getElementById("swi");
-            div.children[2].innerHTML=html3;
+            div.children[3].innerHTML=html3;
             var html4="";
 	    	for(var i=3;i<result.length-1;i++){
 	        	var p=result[i];
@@ -49,7 +60,7 @@ function add(){
                     `;
        	}
             var div=document.getElementById("swi");
-            div.children[3].innerHTML=html4;
+            div.children[4].innerHTML=html4;
             var html5="";
 	    	for(var i=4;i<result.length-0;i++){
 	        	var p=result[i];
@@ -60,15 +71,26 @@ function add(){
                     `;
        	}
             var div=document.getElementById("swi");
-            div.children[4].innerHTML=html5;
+            div.children[5].innerHTML=html5;
             
+            var html6="";
+	    	for(var i=0;i<result.length-4;i++){
+	        	var p=result[i];
+	        	html5+=`
+                    <a href="${p.href}" target="_blank">
+                        <img src="${p.img}">
+                    </a>
+                    `;
+       	}
+            var div=document.getElementById("swi");
+            div.children[6].innerHTML=html6;
 	    }
        
 	} 
     xhr.open('get','/product/carousel',true);
     xhr.send(null);
 }     
-add(); 
+ 
 // 轮播  
 
 //首页商品
